f901e8fd9163123b99b9b0fd0c466c67195552de
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-03 16:12:13 +0100

    Updated FunctionMultipleResult

--- FunctionMultipleResults/main.go
+++ FunctionMultipleResults/main.go
@@ -6,15 +6,22 @@ func tryMeForSingleResult(x, y int) int {
 	return x * y
 }
 
-func tryMeForMultipleResults(x, y int) (int, int) {
-	return x * x, y * y 
+func tryMeForMultipleResults(x, y int) (int, int, string) {
+	return x * x, y * y, fmt.Sprintf("%v%v", x, y)
 }
 
 func main() {
-	singleResult := tryMeForSingleResult(2,6)
+	x := 3
+	y := 7
+
+	fmt.Println("param x:\t\t", x)
+	fmt.Println("param y:\t\t", y)
+
+	singleResult := tryMeForSingleResult(x,y)
 	fmt.Println("singleResult:\t\t", singleResult)
 
-	xPow, yPow := tryMeForMultipleResults(3,7)
+	xPow, yPow, concatenated := tryMeForMultipleResults(x,y)
 	fmt.Println("multipleResults x:\t", xPow)
 	fmt.Println("multipleResults y:\t", yPow)
+	fmt.Println("multipleResults concat:\t", concatenated)
 }
