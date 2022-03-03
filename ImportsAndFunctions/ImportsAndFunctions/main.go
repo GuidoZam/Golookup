15a58422a4889b2031e1613cb90ecf15f64144cc
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-03 15:52:10 +0100

    Added ImportsAndFunctions

---
+++ ImportsAndFunctions/ImportsAndFunctions/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	// Write the sum of the two numbers
+	fmt.Println(add(42, 13))
+
+	// Write a constant from the imported package
+	fmt.Println(math.Pi);
+}
