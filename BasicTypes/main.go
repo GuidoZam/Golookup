3bac936b0a464f812d4be99d184263732f50b08f
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-03 16:20:58 +0100

    BasicTypes

---
+++ BasicTypes/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe   		bool       	= false
+	sampleText	string	   	= "This is just an example"
+	MaxInt 		uint64     	= 1<<64 - 1
+	z      		complex128 	= cmplx.Sqrt(-5 + 12i)
+	a			rune		= 12312
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", sampleText, sampleText)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	fmt.Printf("Type: %T Value: %v\n", a, a)
+}
