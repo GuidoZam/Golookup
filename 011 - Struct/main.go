408f98aad764247c3ad9886367ba5eb2ab242533
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-04 15:06:01 +0100

    Added partial constructor to Struct

--- 011 - Struct/main.go
+++ 011 - Struct/main.go
@@ -8,6 +8,12 @@ type Person struct {
 	BirthCity string
 }
 
+type Triangle struct {
+	a int
+	b int
+	c int
+}
+
 func main() {
 	p1 := Person{ "John", "Doe", "" }
 	fmt.Println(p1)
@@ -18,4 +24,19 @@ func main() {
 	p2 := &p1
 	p2.FirstName = "Jane"
 	fmt.Println(*p2)
+
+	// Constructor without values
+	t := Triangle {}
+	fmt.Println(t)
+
+	// Construct with partial variables
+	p3 := Person { FirstName: "Luigi" }
+	fmt.Println(p3)
+
+	p4 := Person { FirstName: "Mario", LastName: "Super" }
+	fmt.Println(p4)
+
+	p5 := Person { BirthCity: "Berlin" }
+	fmt.Println(p5)
+
 }
