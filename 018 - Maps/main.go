63d73c2f4b0dfdcb77fb8704adbe79150f2b3c40
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-05 16:23:42 +0100

    Updated Maps

--- 018 - Maps/main.go
+++ 018 - Maps/main.go
@@ -8,14 +8,27 @@ type Vertex struct {
 
 var m map[string]Vertex
 
+var secondMap = map[string]Vertex{
+	// You can specify the type
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	// Or you can avoid the explicit type
+	"Google": { 37.42202, -122.08408 },
+}
+
 func main() {
 	m = make(map[string]Vertex)
+	// Add entry
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
+	// Add entry
 	m["Meh"] = Vertex{
 		12.3233, 54.10267,
 	}
 	fmt.Println(m["Bell Labs"])
-		fmt.Println(m["Meh"])
+	fmt.Println(m["Meh"])
+
+	fmt.Println(secondMap)
 }
