d6c527fc54ed9013048e9a61383763113d9a50e1
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-15 17:49:18 +0100

    Updated Methods

--- 024-Methods/main.go
+++ 024-Methods/main.go
@@ -1,6 +1,10 @@
 package main
 
-import f "fmt"
+import (
+	f "fmt"
+	"math"
+)
+
 
 type Person struct {
 	FirstName, LastName string
@@ -16,9 +20,34 @@ func GetInitials(p Person) string {
 	return f.Sprintf("%v%v", p.FirstName[0:1], p.LastName[0:1])
 }
 
+// You can also define method on a non structured type
+type MyFloat float64
+
+func (floatVar MyFloat) Abs() float64 {
+	if floatVar < 0 {
+		return float64(-floatVar)
+	}
+	return float64(floatVar)
+}
+
+type MyInt int
+
+func (intVar MyInt) Abs() int {
+	if intVar < 0 {
+		return int(-intVar)
+	}
+	return int(intVar)
+}
+
 func main() {
 	v := Person{ "Jane", "Doe" }
 	f.Println(v.GetFullName())
 	f.Println(GetInitials(v))
+
+	floatVar := MyFloat(-math.Sqrt2)
+	f.Println(floatVar.Abs())
+
+	intVar := MyInt(-234)
+	f.Println(intVar.Abs())
 }
 
