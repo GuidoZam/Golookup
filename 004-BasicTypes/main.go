13a0c92a246872dbd3ae9b1966f26ea4c67faec2
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-15 17:46:14 +0100

    Updated BasicTypes

--- 004-BasicTypes/main.go
+++ 004-BasicTypes/main.go
@@ -9,6 +9,7 @@ var (
 	ToBe   		bool       	= false
 	sampleText	string	   	= "This is just an example"
 	MaxInt 		uint64     	= 1<<64 - 1
+	sampleInt 	int     	= 42
 	z      		complex128 	= cmplx.Sqrt(-5 + 12i)
 	a			rune		= 12312
 )
@@ -17,6 +18,7 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v\n", sampleText, sampleText)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", sampleInt, sampleInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
 	fmt.Printf("Type: %T Value: %v\n", a, a)
 }
