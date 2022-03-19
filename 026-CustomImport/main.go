96cdc2b82b6739510530a6402cca5bd1caab5399
GuidoZam <gudkaronte+lavoro@gmail.com>
2022-03-19 14:48:59 +0100

    Updated CustomImport

--- 026-CustomImport/main.go
+++ 026-CustomImport/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/guidozam/utility"
+	p "github.com/guidozam/utility/person"
 )
 
 func main() {
+	v := p.Person { FirstName: "Jane", LastName: "Doe" }
+	fmt.Println(v)
 	utility.HelloWorld()
 }
